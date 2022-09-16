65740f2c679212780cd7a8b19b2aba84ae47ee43
Agacy junior <juniormartinssom@gmail.com>
2022-09-16 10:15:33 -0300

    Final Round

---
+++ src/components/DuoMatch/index.tsx
@@ -0,0 +1,77 @@
+import {
+  View,
+  Modal,
+  ModalProps,
+  Text,
+  TouchableOpacity,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import * as Clipboard from "expo-clipboard";
+import { CheckCircle } from "phosphor-react-native";
+
+import { THEME } from "../../theme";
+import { styles } from "./styles";
+import { Heading } from "../Heading";
+import { useState } from "react";
+
+interface Props extends ModalProps {
+  discord: string;
+  onClose: () => void;
+}
+
+export function DuoMatch({ discord, onClose, ...rest }: Props) {
+  const [isCopping, setIsCopping] = useState(false);
+
+  async function handleCopyDiscordUserToClipboard() {
+    setIsCopping(true);
+    await Clipboard.setStringAsync(discord);
+
+    Alert.alert(
+      "Discord cópiado!",
+      "Usuário copiado para você colocar no Discord e encontrar essa pessoa."
+    );
+    setIsCopping(false);
+  }
+
+  return (
+    <Modal transparent statusBarTranslucent animationType="fade" {...rest}>
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <TouchableOpacity style={styles.closeIcon} onPress={onClose}>
+            <MaterialIcons
+              name="close"
+              size={20}
+              color={THEME.COLORS.CAPTION_500}
+            />
+          </TouchableOpacity>
+
+          <CheckCircle size={64} color={THEME.COLORS.SUCCESS} weight="bold" />
+
+          <Heading
+            title="Let's play!"
+            subtitle="Agora é só começar a jogar!"
+            style={{ alignItems: "center", marginTop: 24 }}
+          />
+
+          <Text style={styles.label}>Adicione no Discord</Text>
+
+          <TouchableOpacity
+            style={styles.discordButton}
+            onPress={handleCopyDiscordUserToClipboard}
+            disabled={isCopping}
+          >
+            <Text style={styles.discord}>
+              {isCopping ? (
+                <ActivityIndicator color={THEME.COLORS.PRIMARY} />
+              ) : (
+                discord
+              )}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+}
--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -12,15 +12,24 @@ import { GameParams } from "../../@types/navigation";
 import { THEME } from "../../theme";
 import { Heading } from "../../components/Heading";
 import { DuoCard, DuoCardProps } from "../../components/DuoCard";
+import { DuoMatch } from "../../components/DuoMatch";
 
 export function Game() {
   const [duos, setDuos] = useState<DuoCardProps[]>([]);
+  const [discordDuoSelected, setDiscordDuoSelected] = useState('');
+
   const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
 
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  async function getDiscordUser(adsId: string) {
+    fetch(`http://127.0.0.1:3333/ads/${adsId}/discord`)
+      .then((response) => response.json())
+      .then((data) => setDiscordDuoSelected(data.discord));
   }
 
   useEffect(() => {
@@ -58,7 +67,7 @@ export function Game() {
           data={duos}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
-            <DuoCard data={item} onConnect={() => {}} />
+            <DuoCard data={item} onConnect={() => getDiscordUser(item.id)} />
           )}
           horizontal
           style={styles.containerList}
@@ -71,6 +80,12 @@ export function Game() {
               Não há anúncios publicados ainda.
             </Text>
           )}
+        />
+
+        <DuoMatch 
+          visible={discordDuoSelected.length > 0}
+          discord={discordDuoSelected}
+          onClose={() => setDiscordDuoSelected('')}
         />
       </SafeAreaView>
     </Background>
